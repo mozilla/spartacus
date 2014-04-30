aa0d740f6158f331c09d10dee29de289b2a4cc33
Stuart Colville <scolville@mozilla.com>
2014-04-30 15:11:18 +0000

    Fix cancel from locked + tests (bug 963640)

--- public/js/lib/cancel.js
+++ public/js/lib/cancel.js
@@ -8,26 +8,31 @@ define([
 
   var console = log('cancel');
 
-  var callPayFailure = function callPayFailure() {
+  var pollPaymentFailed = function pollPaymentFailed() {
     // Bug 872987 introduced the injection of the "paymentSuccess" and
     // "paymentFailed" functions within a "mozPaymentProvider" object
     // instead of injecting them in the global scope. So we need to support
     // both APIs.
     var mozPayProvider = window.mozPaymentProvider || {};
     var paymentFailed = mozPayProvider.paymentFailed || window.paymentFailed;
+
     // After Bug 843309 landed, there should not be any delay before the
     // mozPaymentProvider API is injected into scope, but we keep the
     // polling loop as a safe guard.
-    throbber.show(i18n.gettext('Payment Cancelled'));
-
     if (typeof paymentFailed === 'undefined') {
       console.log('waiting for paymentFailed to appear in scope');
-      window.setTimeout(callPayFailure, 500);
+      window.setTimeout(pollPaymentFailed, 500);
     } else {
       console.log('payment failed, closing window');
       paymentFailed('USER_CANCELLED');
     }
   };
+
+
+  function callPayFailure() {
+    throbber.show(i18n.gettext('Payment Cancelled'));
+    pollPaymentFailed();
+  }
 
   return {
     callPayFailure: callPayFailure

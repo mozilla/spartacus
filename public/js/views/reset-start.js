b438af8335c1313559ca40d8fc7454914a2a29de
Stuart Colville <scolville@mozilla.com>
2014-05-15 15:32:58 +0000

    Add reset-start page

---
+++ public/js/views/reset-start.js
@@ -0,0 +1,38 @@
+define([
+  'log',
+  'views/base',
+], function(log, BaseView){
+
+  'use strict';
+
+  var console = log('view', 'reset-start');
+
+  var ResetStartView = BaseView.extend({
+
+    events: {
+      'click .back': 'handleBack',
+      'click .cta:enabled': 'handleResetStart',
+    },
+
+    handleBack: function(e) {
+      e.preventDefault();
+      app.router.navigate('enter-pin', {trigger: true});
+    },
+
+    handleResetStart: function(e) {
+      e.preventDefault();
+      console.log('noop start reset');
+    },
+
+    render: function(){
+      console.log('rendering reset-start view');
+      this.setTitle(this.gettext('Reset your Pin?'));
+      this.renderTemplate('reset-start.html');
+      app.throbber.hide();
+      return this;
+    }
+
+  });
+
+  return ResetStartView;
+});

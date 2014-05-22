10ddd0b2b726d87be2aee3f75d41a2789655173e
Stuart Colville <scolville@mozilla.com>
2014-05-22 11:22:11 +0000

    Add events to was-locked (bug 1014204)

---
+++ tests/ui/test-was-locked-to-enter-pin.js
@@ -0,0 +1,31 @@
+var helpers = require('../helpers');
+
+helpers.startCasper({
+  setUp: function(){
+    helpers.fakeVerification();
+    helpers.fakeStartTransaction();
+    helpers.fakePinData({pin: true, pin_was_locked_out: true});
+  },
+});
+
+casper.test.begin('Test was-locked -> enter-pin', {
+  test: function(test) {
+
+    helpers.doLogin();
+
+    casper.waitForUrl(helpers.url('was-locked'), function() {
+      test.assertVisible('.button.cta', 'CTA button should be shown');
+      test.assertVisible('.button.reset-start', 'Reset pin button should be visible');
+      casper.click('.button.cta');
+    });
+
+    casper.waitForUrl(helpers.url('enter-pin'), function() {
+      test.assertVisible('.pinbox', 'Pin entry widget should be displayed');
+    });
+
+    casper.run(function() {
+      test.done();
+    });
+
+  },
+});
---
+++ tests/ui/test-was-locked-to-reset-start.js
@@ -0,0 +1,31 @@
+var helpers = require('../helpers');
+
+helpers.startCasper({
+  setUp: function(){
+    helpers.fakeVerification();
+    helpers.fakeStartTransaction();
+    helpers.fakePinData({pin: true, pin_was_locked_out: true});
+  },
+});
+
+casper.test.begin('Test was-locked -> reset-start', {
+  test: function(test) {
+
+    helpers.doLogin();
+
+    casper.waitForUrl(helpers.url('was-locked'), function() {
+      test.assertVisible('.button.cta', 'CTA button should be shown');
+      test.assertVisible('.button.reset-start', 'Reset pin button should be visible');
+      casper.click('.button.reset-start');
+    });
+
+    casper.waitForUrl(helpers.url('reset-start'), function() {
+      test.assertVisible('.reset-start', 'Reset start content should be visible.');
+    });
+
+    casper.run(function() {
+      test.done();
+    });
+
+  },
+});

94a0d25d70d028d45a5a4303e5bc6e7a9d1f8e58
Stuart Colville <scolville@mozilla.com>
2014-06-26 17:38:27 +0000

    Provide settings override from data-attr (bug 1030796)

--- tests/static/testlib/testrunner.js
+++ tests/static/testlib/testrunner.js
@@ -1,26 +1,21 @@
 require(['/js/require-config.js'], function() {
 
+  mocha.setup('tdd');
+
   require([
-    'require',
-    'main',
-  ], function(require){
-
-    mocha.setup('tdd');
-
-    require([
-      '/unit/test-base-view.js',
-      '/unit/test-utils.js',
-      '/unit/test-errors.js',
-      '/unit/test-mcc-mnc.js',
-      '/unit/test-locale-utils.js',
-      '/unit/test-persona-config.js'
-    ], function() {
-      if (window.mochaPhantomJS) {
-        window.mochaPhantomJS.run();
-      } else {
-        window.mocha.run();
-      }
-    });
+    '/unit/test-base-view.js',
+    '/unit/test-errors.js',
+    '/unit/test-locale-utils.js',
+    '/unit/test-mcc-mnc.js',
+    '/unit/test-persona-config.js',
+    '/unit/test-settings.js',
+    '/unit/test-utils.js'
+  ], function() {
+    if (window.mochaPhantomJS) {
+      window.mochaPhantomJS.run();
+    } else {
+      window.mocha.run();
+    }
   });
 
 });

ae65200962c76404224e7d4911e2c072c9d3a234
Stuart Colville <scolville@mozilla.com>
2014-07-09 15:31:33 +0000

    Remove more messages

--- public/js/error-codes.js
+++ public/js/error-codes.js
@@ -28,20 +28,8 @@ define([
     PIN_RESET_NO_USER: gettext('No valid user found whilst attempting to reset your PIN'),
     PIN_RESET_PERM_DENIED: gettext('Permission denied attempting to reset your PIN.'),
     PIN_RESET_TIMEOUT: gettext('The request timed out whilst attempting to reset your PIN.'),
-    PIN_STATE_ERROR: gettext('An unexpected error occurred whilst fetching data.'),
-    PIN_STATE_TIMEOUT: gettext('The request timed out fetching data.'),
-    REAUTH_LOGOUT_ERROR: gettext('An error occurred whilst trying to log out.'),
-    REVERIFY_DENIED: gettext('Permission denied re-verifying the user.'),
-    REVERIFY_FAILED: gettext('Re-verifying the user failed.'),
-    REVERIFY_MISSING_PROVIDER: gettext('The provider did not exist'),
-    REVERIFY_MISSING_URL: gettext('The re-verification url is not configured.'),
-    REVERIFY_TIMEOUT: gettext('The request to the server timed out during re-verification.'),
     TRANS_FAILED: gettext('The transaction failed. You have not been charged for this purchase.'),
-    TRANS_TIMEOUT: gettext('The system timed out while waiting for a transaction.'),
-    UNEXPECTED_ERROR: gettext('An unexpected error occurred.'),
-    UNEXPECTED_STATE: gettext('An unexpected error occurred.'),
     UNSUPPORTED_PAY: gettext('The payment method or price point is not supported for this region or operator.'),
-    USER_CANCELLED: gettext('The user cancelled the payment.'),
   };
 
 });

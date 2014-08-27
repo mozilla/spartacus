c465c05f4c20e21aa3e5a0277ccb0cc76304773f
Stuart Colville <scolville@mozilla.com>
2014-08-27 10:35:49 +0000

    Pass error code to cancel function (bug 1050544)

--- public/js/lib/cancel.js
+++ public/js/lib/cancel.js
@@ -8,13 +8,13 @@ define([
 
   var console = log('cancel');
 
-  function callPayFailure(e) {
+  function callPayFailure(e, errorCode) {
     if (e) {
       e.preventDefault();
     }
     app.error.close();
     app.throbber.render(i18n.gettext('Payment cancelled.'));
-    var errorCode = utils.bodyData.errorCode || 'USER_CANCELLED';
+    errorCode = errorCode || 'USER_CANCELLED';
     console.log('Running paymentFailed with errorCode: ' + errorCode);
     utils.mozPaymentProvider.paymentFailed(errorCode);
   }

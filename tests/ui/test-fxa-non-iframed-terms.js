59aca54dec2df44fc85d606abb70275b391f458f
Stuart Colville <scolville@mozilla.com>
2014-10-15 14:56:36 +0100

    Commenting out popup checks as they are very unreliable (bug 1077568)

--- tests/ui/test-fxa-non-iframed-terms.js
+++ tests/ui/test-fxa-non-iframed-terms.js
@@ -25,20 +25,21 @@ casper.test.begin('Check FxA terms links', {
       test.assertVisible('.pinbox', 'Pin entry widget should be displayed');
       test.assertVisible('.terms', 'Terms and privacy policy urls should be present.');
       casper.click('.tos');
+      test.assertDoesntExist('.iframe-overlay', "Check iframe isn't created");
     });
 
-    casper.waitForPopup('https://marketplace.firefox.com/terms-of-use', function() {
-      casper.echo('Terms opened via window.open');
-    });
-
-    casper.withPopup('https://marketplace.firefox.com/terms-of-use', function() {
-      this.waitForSelector('.site-terms-of-use', function() {
-        this.test.assertExists('.site-terms-of-use', 'Check terms page exists');
-        this.evaluate(function() {
-          window.close();
-        });
-      });
-    });
+//    casper.waitForPopup('https://marketplace.firefox.com/terms-of-use', function() {
+//      casper.echo('Terms opened via window.open');
+//    });
+//
+//    casper.withPopup('https://marketplace.firefox.com/terms-of-use', function() {
+//      this.waitForSelector('.site-terms-of-use', function() {
+//        this.test.assertExists('.site-terms-of-use', 'Check terms page exists');
+//        this.evaluate(function() {
+//          window.close();
+//        });
+//      });
+//    });
 
     casper.run(function() {
       test.done();

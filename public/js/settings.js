445f8ac4998b330c929c94efe0261e808def710a
Kumar McMillan <kumar.mcmillan@gmail.com>
2014-11-26 11:23:54 -0600

    Disable fxA in prod (bug 1105076)
    We discovered that the first email hashed for a uuid
    is the raw email (from the marketplace JWT) but subsequent
    emails are tokenized fxA emails. This causes webpay to send the
    wrong user uuid to Bango. Until that's fixed, we're
    disabling fxA in prod.

--- public/js/settings.js
+++ public/js/settings.js
@@ -110,7 +110,7 @@ var settings = {
     'https://zippy-dev.allizom.org',
     'https://zippy.paas.allizom.org'
   ],
-  enableNativeFxA: true
+  enableNativeFxA: false
 };
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {

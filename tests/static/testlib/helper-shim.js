906fd5990431c6496aa690776061f954c2d001ca
Stuart Colville <scolville@mozilla.com>
2015-02-19 10:36:06 +0000

    Show phantom version and DRY up helper-shim

--- tests/static/testlib/helper-shim.js
+++ tests/static/testlib/helper-shim.js
@@ -1,13 +1,15 @@
+var v;
 if (require.globals) {
   // SlimerJS
   require.globals.casper = casper;
   casper.echo('Running under SlimerJS', 'WARN');
-  var v = slimer.version;
-  casper.echo('Version: ' + v.major + '.' + v.minor + '.' + v.patch);
-  var helpers = require(require('fs').absolute('tests/helpers'));
+  v = slimer.version;
   casper.isSlimer = true;
 } else {
   // PhantomJS
   casper.echo('Running under PhantomJS', 'WARN');
-  var helpers = require('../helpers');
+  v = phantom.version;
 }
+casper.echo('Version: ' + v.major + '.' + v.minor + '.' + v.patch);
+/* exported helpers */
+var helpers = require(require('fs').absolute('tests/helpers'));
